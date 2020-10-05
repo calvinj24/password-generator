e3beee547fd72b47b50e65022922e98fc89e5b01
Calvin Johnson <calvinjohnson@Calvins-MBP-7.attlocal.net>
2020-10-04 22:28:54 -0500

    added more characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,8 @@
 // Assignment code here
-var lowercaseArray = ["a","b","c"];
-var uppercaseArray = ["A","B","C"];
-var numericArray = [1,2,3];
-var specialArray = ["$","&","!"]
+var lowercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var uppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numericArray = [1,2,3,4,5,6,7,8,9,0];
+var specialArray = ["$","&","!","#","@","%","^","*",]
 
 
 
